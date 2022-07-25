804e1887d96d148838a35833d22ff23d457d8149
David Vargas <dvargas92495@gmail.com>
2022-07-25 16:16:20 -0400

    Add required providers

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.7.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "4.2.0"
+    }
+  }
+}
+
 data "aws_caller_identity" "current" {}
 
 variable "name" {
