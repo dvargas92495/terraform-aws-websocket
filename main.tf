88a0dc7e9b957274bcfdac36a4f0e7fd60eda819
David Vargas <dvargas92495@gmail.com>
2022-11-28 02:03:30 -0500

    node 18

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_lambda_function" "websocket_lambda" {
   function_name = "${var.name}_ws_${each.value}"
   role          = aws_iam_role.lambda_role.arn
   handler       = "ws_${each.value}.handler"
-  runtime       = "nodejs16.x"
+  runtime       = "nodejs18.x"
   timeout       = 10
   memory_size   = 5120
 }
